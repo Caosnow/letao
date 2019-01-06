5436eeb98596de7fa13704e8591cc061d24e6d12
Caosnow <635624470@qq.com>
2019-01-06 11:01:09 +0800

    用户管理页,模态框,及其功能全部完成

--- public/manage01/js/user.js
+++ public/manage01/js/user.js
@@ -2,6 +2,10 @@
 //已进入页面发送ajax请求,利用引擎模板,动态渲染数据
 
 $(function(){
+    //当前操作用户的id 
+    var currentId;
+    //当前需要修改用户的状态
+    var isDelete;
 
     // 在全局声明一个页数来记录
     var currentPage = 1;
@@ -9,6 +13,7 @@ $(function(){
     var pageSize = 5;
 
     render();
+
     function render(){
         $.ajax({
             type:'get',
@@ -77,11 +82,44 @@ $(function(){
 
 
 
+        // 3  给所有的操作栏  按钮注册事件  ,由于是动态渲染的,所以要 使用  事件委托 注册事件
+
+        $('tbody').on('click','.btn',function(){
+
+            //点击按钮,显示模态框
+            $('#usertModal').modal('show');
+             //获取当前用户的id 
+             currentId = $(this).parent().data("id");
+             console.log(currentId);
+             //当前需要修改用户的状态   // 获取将用户修改成什么状态  // 禁用按钮 ? 禁用状态 0 : 启用状态 1
+            //如果是禁用,按钮是启用状态 1 ,如果是正常,按钮是禁用状态0
+             isDelete = $(this).hasClass('btn-danger')? 0: 1;
+        })
 
 
-
-
-
+        // 4 给模态框  确认按钮 注册点击事件 
+        //发送ajax请求,
+        $('.submitBtn').click(function(){
+           
+            $.ajax({
+                url:'/user/updateUser',
+                type:'post',
+                data:{
+                    id:currentId,
+                    isDelete:isDelete
+                },
+                datatype:'json',
+                success:function(info){
+                    console.log(info);
+                    if(info.success){
+                        //关闭模态框  ,就是隐藏模态框
+                        $('#usertModal').modal('hide');
+                        //重新渲染页面
+                        render();
+                    }
+                }
+            })
+        })
 
 
 })
